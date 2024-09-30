63310433fe548a7a6f9b1c4c88275dfc8f309bcb
Nkeonyeasua-Odita <oditagift@gmail.com>
2024-09-30 00:08:01 -0400

    Added more week1-code

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -1,5 +1,6 @@
 package com.bptn.course._05_strings;
 
+import java.util.Scanner;
 public class Strings {
 
 	public static void main(String[] args) {
@@ -19,15 +20,15 @@ public class Strings {
 		
 		//print the count
 		
-		String userName = "This is a test";
-		//System.out.println(userName);
-		System.out.println(userName.length()); // the lenght of the spring
-		//System.out.println(userName.charAt(2));//2 here is index , s
-				System.out.println(userName.substring(0,3)); // same function starts at index 2 and ends before 4
-		//System.out.println(userName.trim());// takes out extra spaces on both ends but not in between helpful for scanner inputs
-		System.out.println(userName.indexOf("Hello")); //This will  return -1
-		
-		System.out.println(userName.substring(5)); //substring is the character you want to extract 0-3
+//		String userName = "This is a test";
+//		//System.out.println(userName);
+//		System.out.println(userName.length()); // the lenght of the spring
+//		//System.out.println(userName.charAt(2));//2 here is index , s
+//				System.out.println(userName.substring(0,3)); // same function starts at index 2 and ends before 4
+//		//System.out.println(userName.trim());// takes out extra spaces on both ends but not in between helpful for scanner inputs
+//		System.out.println(userName.indexOf("Hello")); //This will  return -1
+//		
+//		System.out.println(userName.substring(5)); //substring is the character you want to extract 0-3
 
 		//String message1 = "This is a test";
 		
@@ -37,12 +38,12 @@ public class Strings {
 //		System.out.println(s2);
 //		System.out.println(s2.substring(0,4));
 //		
-		
-		String s1 = "Hi";
-		String s2 = "Bye";
-		int ans1 = s1.compareTo(s2);
-		System.out.println(ans1 + 4);
-		
+//		
+//		String s1 = "Hi";
+//		String s2 = "Bye";
+//		int ans1 = s1.compareTo(s2);
+//		System.out.println(ans1 + 4);
+//		
 		//message1.indexOf("Helypplo");// output is nothing / 0 which means st
 		
 		
@@ -56,9 +57,36 @@ public class Strings {
 //		System.out.println(userName.trim());// takes out extra spaces on both ends but not in between helpful for scanner inputs
 //		System.out.println(userName.indexOf('r'));//Returns the index within this string of the first occurrence ofthe specified character
 		
+// writes a program that takes a strin as input and counts the number of vowels and consonants inthe string
+		// consider both uppercase and lowercase letters
+		//input: strings entered by user and ouput: the number of vowles and consonant
 		
+		//read user input
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter a string: ");
+		String input = scan.nextLine();
 		
-		
-		
+		//declare storage variables
+		int vowelCount = 0;
+		int consonantCount = 0;
+//		convert evrything to lowercase or upper your choice
+		String lowerCaseInput = input.toLowerCase();
+//		loop through each character in the string
+		for(int i=0; i<lowerCaseInput.length(); i++) {//i here is for the indexes
+			char ch = lowerCaseInput.charAt(i);//read character by character
+//			check if its vowel or consonant
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vowelCount++;
+				// if you go straight to else execution you it will count special charcters as
+				// vowel because else
+				// enter condition for just alphabets
+			} else if (ch >= 'a' && ch <= 'z') {// its like saying i>=1 <=10
+				// update count
+				consonantCount++;
+			}
+		}
+//		print count
+		System.out.println("The number of vowels are: " + vowelCount);
+		System.out.println("The number of cosonants are: " + consonantCount);
 	}
 }
---
+++ src/main/java/com/bptn/course/_examples_led/WeekOne.java
@@ -0,0 +1,51 @@
+package com.bptn.course._examples_led;
+
+public class WeekOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// STRINGS
+
+//		String start = "Happy Birthday";
+//
+//		String fullName = "John Doe";
+//		String result = start + " " + fullName; // add together strings
+//		result += "!!!"; // add on to the same string
+//		System.out.println(result);
+//
+//		String s1 = "xy";
+//		String s2 = s1;
+//		s1 = s1 + s2 + "z";
+//		System.out.println(s1);
+//		System.out.println(s2 + "game time" + " is here" + "\"NOW\""); 
+//		// TO put quote in print() use \" word \"
+
+//		String message1 = "This is a test";
+//		String message2 = "Hello Class";
+//		String message = "Hello!";
+//
+//		System.out.println(message1.length()); //
+//		System.out.println(message2.length());
+//
+//		System.out.println(message1.substring(0, 3));//PRINTS FROM INDEX 0-2(( 
+//		System.out.println(message2.substring(4, 5));//PRINT INDEX 4
+//		System.out.println(message1.substring(5));
+//
+//		System.out.println(message1.indexOf("is")); // INDEXOF FINDS A CHARRACTER IN THE STRING AND OUTPUS THE INDEX NUMBER IT STARTS FROM 
+//		//A This will match the is in "This"! AS ITS THE FIRST IS
+//		System.out.println(message1.indexOf("Hello"));// THIS WILL OUTPU -1 BECAUSE ITS NOT FOUND IN THE STRING
+//		System.out.println(message2.indexOf("Hello"));
+//
+//		System.out.println(message2.toLowerCase());
+//		System.out.println(message2.toUpperCase());
+//		System.out.println(message2.compareTo(message1));// returns -12 H COMES EARLIER THAN T SO ITS -12. 
+//		System.out.println(message2.compareTo(message1));//THIS RETURNS 12
+//		//compare if the first string is alphabetically ordered after the second string, it returns a positive number. if not -1
+//		
+//		// use this strings method for case sensitive strings so it works well compareToIgnoreCase and equalsIgnoreCase.
+
+//		System.out.println(str1.substring((str1.length()) - 1));
+
+	}
+
+}
